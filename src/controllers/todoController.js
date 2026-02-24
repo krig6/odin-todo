3307043c0527c17450cd8c41a9326355d63f3d1f
krig6 <ej.ldsm@gmail.com>
2026-02-24 15:59:47 +0800

    fix(controller): add list parameter to addTodo
    - Accepts `list` as first argument to append new todo
    - Keeps consistent signature with other methods
    - Returns new array with the created todo appended

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -2,12 +2,12 @@ import { createTodo } from "../models/todo.js"
 
 export const todoController = () => {
 
-  const addTodo = ({
+  const addTodo = (list, {
     title,
     description,
     dueDate,
     priority,
-    status }) => createTodo(title, description, dueDate, priority, status)
+    status }) => [...list, createTodo(title, description, dueDate, priority, status)]
 
   const removeTodo = (list, todoId) => list.filter(todo => todo.id !== todoId)
 
