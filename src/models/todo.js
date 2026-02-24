32095db5485f7b45dd34e4b401a560fa97ea5555
krig6 <ej.ldsm@gmail.com>
2026-02-24 11:53:24 +0800

    feat(model): add createTodo factory function
    - Creates todo object with default values
    - Generates unique id using `crypto.randomUUID()`
    - Defaults `dueDate` to tomorrow if not provided using `date-fns`
    - Sets default priority to `high` and status to `inProgress`

---
+++ src/models/todo.js
@@ -0,0 +1,12 @@
+import { addDays } from "date-fns"
+
+export const createTodo = (title = 'Untitled', description = '', dueDate, priority = 'high', status = 'inProgress') => {
+  return {
+    id: crypto.randomUUID(),
+    title,
+    description,
+    dueDate: dueDate ?? addDays(new Date(), 1),
+    priority,
+    status
+  }
+}
